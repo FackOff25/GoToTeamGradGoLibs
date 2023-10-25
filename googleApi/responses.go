60a562a59db8a6938c89df9185ff6d869bf00e20
FackOff25 <kozvladimir@bk.ru>
2023-10-25 15:36:19 +0300

    nearbyPlaces answer

--- googleApi/responses.go
+++ googleApi/responses.go
@@ -1,16 +1,21 @@
 package googleApi
 
 const (
-	STATUS_OK = "OK"
-	STATUS_ZERO_RESULTS = "ZERO_RESULTS"
-	STATUS_NOT_FOUND = "NOT_FOUND"
-	STATUS_INVALID_REQUEST = "INVALID_REQUEST"
+	STATUS_OK               = "OK"
+	STATUS_ZERO_RESULTS     = "ZERO_RESULTS"
+	STATUS_NOT_FOUND        = "NOT_FOUND"
+	STATUS_INVALID_REQUEST  = "INVALID_REQUEST"
 	STATUS_OVER_QUERY_LIMIT = "OVER_QUERY_LIMIT"
-	STATUS_REQUEST_DENIED = "REQUEST_DENIED"
-	STATUS_UNKNOWN_ERROR = "UNKNOWN_ERROR"
+	STATUS_REQUEST_DENIED   = "REQUEST_DENIED"
+	STATUS_UNKNOWN_ERROR    = "UNKNOWN_ERROR"
 )
 
 type PlaceAnswer struct {
 	Status string `json:"status"`
-	Result Place `json:"result"`
+	Result Place  `json:"result"`
+}
+
+type NearbyPlacesAnswer struct {
+	Status string  `json:"status"`
+	Result []Place `json:"results"`
 }
